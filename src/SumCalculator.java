6335e534560845928377b8adfa208a040049f265
Nishan Thapa <nishanthapa855@gmail.om>
2021-08-27 01:33:11 -0500

    commit 11

--- src/SumCalculator.java
+++ src/SumCalculator.java
@@ -1,2 +1,17 @@
+/////////////////////////////////////////////////////////////
+//          1. Find Sum
+////////////////////////////////////////////////////////////
 public class SumCalculator {
+
+    public static void main(String [] args){
+
+        //given hard-coded value
+        int a= 100;
+        double b =300.00;
+
+        //preserving the two decimal precesion while printing
+        System.out.printf("The Sum is %.2f",a+b);
+
+
+    }
 }
